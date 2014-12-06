8f74cd4dc9848db1fd9a489ec9df89a06738bb10
glocalsaint <glocalsaint@users.noreply.github.com>
2014-12-06 12:30:05 +0000

    Update fifo.c

--- fifo.c
+++ fifo.c
@@ -18,53 +18,49 @@ static int last =0;
 static int x=30;
 static void queueReadWrite(int isRead, char *readBuffer, const char *writtenBuf, size_t count)
 {
-printk(KERN_ALERT "Inside the x=%d:%s function\n",x, __FUNCTION__);
-mutex_lock(&my_mutex);
-if(isRead==1)
-{
-        copy_to_user(readBuffer,stringPointer[first],strlen(stringPointer[first]));
+        printk(KERN_ALERT "Inside the x=%d:%s function\n",x, __FUNCTION__);
+        mutex_lock(&my_mutex);
+        if(isRead==1)
+        {
+                copy_to_user(readBuffer,stringPointer[first],strlen(stringPointer[first]));
+        }
+        else
+        {
+                //copy_from_user(stringPointer[first],writtenBuf, count);
+                printk(KERN_ALERT "Inside the %s function(Write part) with string %s, count:%d\n",x, __FUNCTION__,writtenBuf,count);
+        }
+        mutex_unlock(&my_mutex);
 }
-else
+static ssize_t fifo_read(struct file *file, char *buf, size_t count, loff_t *ppos)
 {
-        //copy_from_user(stringPointer[first],writtenBuf, count);
-        printk(KERN_ALERT "Inside the %s function(Write part) with string %s, count:%d\n",x, __FUNCTION__,writtenBuf,count);
-
-}
-mutex_unlock(&my_mutex);
-}
-static ssize_t fifo_read(struct file *file, char *buf, size_t count,
-                                                        loff_t *ppos)
-{
-printk(KERN_ALERT "Inside the x=%d:%s function\n",x, __FUNCTION__);
-x=x+1;
-queueReadWrite(1,buf,NULL,0);
-return 0;
+        printk(KERN_ALERT "Inside the x=%d:%s function\n",x, __FUNCTION__);
+        x=x+1;
+        queueReadWrite(1,buf,NULL,0);
+        return 0;
 }
 
 // this method is executed when writing to the module
-static ssize_t fifo_write( struct file *file, const char *buf, size_t count,
-                                                 loff_t *ppos )
+static ssize_t fifo_write( struct file *file, const char *buf, size_t count, loff_t *ppos )
 {
-
-printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
-queueReadWrite(0,NULL,buf,count);
-return 0;
+        printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__)     ;
+        queueReadWrite(0,NULL,buf,count);
+        return 0;
 }
 
 // this method is called whenever the module is being used
 // e.g. for both read and write operations
 static int fifo_open(struct inode * inode, struct file * file)
 {
-printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
-return 0;
+        printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+        return 0;
 }
 
 // this method releases the module and makes it available for new
 // operations
 static int fifo_release(struct inode * inode, struct file * file)
 {
-printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
-return 0;
+        printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+        return 0;
 }
 
 // module's file operations
@@ -79,16 +75,16 @@ static struct file_operations fifo_fops = {
 // initialize module (executed when using insmod)
 static int __init fifo_init(void)
 {
-printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
-mutex_init(&my_mutex); /* called only ONCE */
-register_chrdev(240,"FIFO",&fifo_fops);
-return 0;
+        printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+        mutex_init(&my_mutex); /* called only ONCE */
+        register_chrdev(240,"FIFO",&fifo_fops);
+        return 0;
 }
 
 // cleanup module (executed when using rmmod)
 static void __exit fifo_cleanup(void)
 {
-unregister_chrdev(240,"FIFO");
+        unregister_chrdev(240,"FIFO");
 }
 
 module_init(fifo_init);
